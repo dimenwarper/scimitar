28a408dabc89bf9e18daff6031ca0ca85f648020
Pablo Cordero <dimenwarper@gmail.com>
2016-05-18 17:31:04 -0700

    Reorganizing repo

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from scimitar import *
+from src import coexpression, model_comparisons, pipelines, differential_analysis, models, plotting, utils, morphing_mixture
 
 __all__ = ['coexpression', 'model_comparisons','pipelines',
            'differential_analysis','models',' plotting','utils',
