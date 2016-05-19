a5c451b6f2010510fbbf3db610bf76daa12dc8f1
Pablo Cordero <dimenwarper@gmail.com>
2016-05-18 17:32:00 -0700

    More reorg

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-from src import coexpression, model_comparisons, pipelines, differential_analysis, models, plotting, utils, morphing_mixture
-
 __all__ = ['coexpression', 'model_comparisons','pipelines',
            'differential_analysis','models',' plotting','utils',
            'morphing_mixture']
